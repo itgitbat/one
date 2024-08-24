81542a33ecaa82fd2d1d414bdb1f7dba27a0095b
itgitbat <kj@purelymail.com>
2024-08-24 01:35:33 +0000

    Create makeIcon.bat

---
+++ etc/makeIcon.bat
@@ -0,0 +1,5 @@
+@                                                                                                                                                                                                                                                                                                                       echo off
+
+
+i_view64.exe /resize="(300,533)" /aspectratio /resample /convert=*.ico /file=%*
+
